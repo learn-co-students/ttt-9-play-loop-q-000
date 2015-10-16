0a20ef45f20e346c3764b8dd82f2788a07f885f8
ElnurIbrahimzade <elnur.ibrahim-zade@hotmail.com>
2015-10-16 12:42:46 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -31,3 +31,18 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+
+  turn(board)
+  turn(board)
+  turn(board)
+  
+  turn(board)
+  turn(board)
+  turn(board)
+  
+  turn(board)
+  turn(board)
+  turn(board)
+
+end
