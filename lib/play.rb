78168541dc1e330ce3dcb05280ba7d3044805f9c
gmichnikov <greg.michnikov@gmail.com>
2015-10-21 22:48:07 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -19,11 +19,17 @@ def valid_move?(board, position)
   position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
 end
 
-def turn(board)
+def turn(board, counter)
   puts "Please enter 1-9:"
+  if counter%2==1 
+    char = "X"
+  else
+    char = "O"
+  end
+  
   input = gets.strip
   if valid_move?(board, input)
-    move(board, input)
+    move(board, input, char)
   else
     turn(board)
   end
@@ -31,3 +37,10 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  counter = 1;
+  while counter <= 9
+    turn(board, counter)
+    counter+=1
+  end
+end
