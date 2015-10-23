b3809ca2a3b2d481a28c30112ee1487ad4ee5818
DakotaLMartinez <dakotaleemusic@gmail.com>
2015-10-23 01:59:06 +0000

    adds the play method to loop through turns

--- lib/play.rb
+++ lib/play.rb
@@ -30,4 +30,10 @@ def turn(board)
   display_board(board)
 end
 
-# Define your play method below
+def play(board)
+  moves_left = 9
+  while moves_left > 0
+    turn(board)
+    moves_left -= 1
+  end
+end
