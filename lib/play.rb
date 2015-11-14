2fe714323bdd51f4487249dbee3f7803402b1f29
LKWLaLa <lkwlala@gmail.com>
2015-11-14 18:17:10 +0000

    nitrous-backup at Sat Nov 14 18:17:10 UTC 2015

--- lib/play.rb
+++ lib/play.rb
@@ -7,7 +7,24 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move(board, location, current_player = "X")
+def turn_count(board)
+  count = 0
+  board.each do |position|
+    if position != "" && position != " " && position != nil
+      count +=1
+    end
+  end
+  return count
+end
+
+
+def current_player(board)
+  counted = turn_count(board)
+  (counted == 0 ||counted % 2 == 0)? "X" : "O"
+end
+
+
+def move(board, location, current_player = current_player(board))
   board[location.to_i-1] = current_player
 end
 
@@ -30,13 +47,54 @@ def turn(board)
   display_board(board)
 end
 
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],  # Bottom row
+  [0,4,8],  # Top left diagonal
+  [2,4,6],  # Top right diagonal
+  [0,3,6],  # first column
+  [1,4,7],  # middle column
+  [2,5,8]  # last column
+  ]
+
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |line|
+    line.all?{|i| board[i] == "X" } || line.all?{|i| board[i] == "O"}
+  end
+end
+
+def full?(board)
+  board.all?{|position| position == "X" || position == "O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+  end
+
+def winner(board)
+  if won?(board)
+  line = won?(board)
+  board[line[0]]
+  end
+end
+
+
 # Define your play method below
 
 def play(board)
-  count = 0
-  while count < 9
+  while !over?(board)
     turn(board)
-    count += 1
+  end
+  if won?(board)
+    puts "Congratulations, #{winner(board)}!"
+  else
+    puts "It's a draw!"
   end
 end
 
