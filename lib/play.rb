3fa932f41fe9a0e66fdf5cea7ee4a132a3e2d751
reifnotreef <bridgerrhammond@gmail.com>
2015-11-24 18:46:54 +0000

    nitrous-backup at Tue Nov 24 18:46:54 UTC 2015

--- lib/play.rb
+++ lib/play.rb
@@ -30,6 +30,18 @@ def turn(board)
   display_board(board)
 end
 
+
+# Def turn_count to keep track of turn #
+def turn_count(board) # determine what turn it is
+  turn_num = 0
+  board.each do |mark|
+    if mark == "X" || mark == "O"   # For each "X" or "O" in (board)
+      turn_num += 1  # add 1 to turn_num
+    end
+  end
+  return turn_num # return current turn_num
+end
+
 # Define your play method below
 def play(board)
   turn_num = 0
