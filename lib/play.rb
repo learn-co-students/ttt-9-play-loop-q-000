f10021e210f6ff4615c13f1bac695c88fbcfaf41
Juan Ruiz <juankboards@hotmail.com>
2015-12-10 13:22:15 +0000

    nitrous-backup at Thu Dec 10 13:22:15 UTC 2015

--- lib/play.rb
+++ lib/play.rb
@@ -33,5 +33,21 @@ end
 def play (board)
   9.times do
     turn(board)
-  end  
+  end
 end
+
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+end
+
+def current_player(board)
+  turn_count(board)%2==0? "O" : "X"
+end
+  
+
+
