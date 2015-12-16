d54e625724470df966e172ed73ea99b73cf15c9b
lamass <hadzy@yahoo.com>
2015-12-16 02:36:39 +0000

    nitrous-backup at Wed Dec 16 02:36:39 UTC 2015

--- lib/play.rb
+++ lib/play.rb
@@ -36,12 +36,21 @@ def play(board)
 until players_turn == 9
      turn(board)
      players_turn += 1
-  end
+end
   puts "This game is over!"
 end
 
+def turn_count(board)
+  turn_count(board) % 2 == 0? "X": "O"
+end
 
-
-
-
-
+def current_player(board)
+  counter = 0
+  board.each do |l|
+    if l == "X" || l == "0"
+      counter += 1
+    end
+  end
+  return counter
+end
+end
