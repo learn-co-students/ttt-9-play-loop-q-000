59856bfc1f87e0a01f0a76baebd3863abf4c5029
jamesmccarthy65 <jamesmccarthy65@gmail.com>
2016-02-15 01:23:31 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -30,4 +30,16 @@ def turn(board)
   display_board(board)
 end
 
-# Define your play method below
+def play(board)
+  count = 0
+  while count <10
+    turn(board)
+    count += 1
+      if count >= 9
+        puts "Thanks for playing!!"
+    break
+      end
+  end
+
+end
+
