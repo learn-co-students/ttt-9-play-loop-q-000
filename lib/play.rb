0ff13751455d1b60957445ac67186ce9f78a6b59
ethanransdell <ethan.ransdell@live.com>
2016-04-14 21:07:00 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -1,33 +1,51 @@
-# Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
+  puts"-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move(board, location, current_player = "X")
-  board[location.to_i-1] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
 def valid_move?(board, position)
-  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+  position_int = position.to_i - 1
+  if position_int.between?(0, 8) && !position_taken?(board, position_int)
+    return true
+  else
+    return false
+  end
+end
+
+def position_taken?(board, position)
+  position_int = position.to_i
+  if board[position_int] == nil || board[position_int] == "" || board[position_int] == " "
+    return false
+  else
+    return true
+  end
+end
+
+def move(board, position, char="X")
+  position_int = position.to_i - 1
+  board[position_int] = char
+  return board
 end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.strip
-  if valid_move?(board, input)
-    move(board, input)
-  else
-    turn(board)
+  position = gets.strip
+  while !valid_move?(board, position)
+    puts "Please enter 1-9:"
+    position = gets.strip
   end
+  board = move(board, position)
   display_board(board)
+  return board
 end
 
+
 # Define your play method below
+def play(board)
+  9.times do
+    board = turn(board)
+  end
+end
