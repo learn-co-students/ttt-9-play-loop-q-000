996c84b34bd423f709977838ec884ed8044a2de2
thejewellster <marcellajewell@gmail.com>
2016-05-04 19:17:13 +0000

    nitrous-backup at Wed May  4 19:17:13 UTC 2016

--- lib/play.rb
+++ lib/play.rb
@@ -36,6 +36,23 @@ def play(board)
     turns+=1
     turn(board)
   end
-  puts "the game is done!"
+end
+
+def turn_count(board)
+    counter = 0
+    board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+      puts "#{counter}" 
+    end
+end
+
+def current_player
+ if turn_count(board) % 2 == 0 #even turn put 0
+   "X"
+ end
+ if turn_count(board) % 2 == 1 #odd turn put 1
+   "O"
+ end
 end
 
