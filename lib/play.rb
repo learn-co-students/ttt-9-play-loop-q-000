dea65d512e94873045cfb344d2d416af596f7f01
John Schlesinger <jschles1@ramapo.edu>
2016-06-01 20:54:20 -0400

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -34,5 +34,11 @@ def turn(board)
     turn(board)
   end
 end
-
 # Define your play method below
+def play(board)
+  turn_number = 0
+  until turn_number == 9
+    turn(board)
+    turn_number += 1
+  end
+end
