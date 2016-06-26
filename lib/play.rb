0dd674d7361921b2c059a352ff667accb64a7805
Mai Vong <mai.ly.vong@gmail.com>
2016-06-26 19:44:15 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -38,11 +38,8 @@ end
 # Define your play method below
 def play(board)
   counter = 0
-  loop do
-    counter += 1
+  while counter < 9
     turn(board)
-    if counter >= 9
-      break
-    end
+    counter += 1
   end
 end
