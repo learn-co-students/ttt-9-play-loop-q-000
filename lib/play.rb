d369e780a1ec430247e2c581cf7bd9682b9032a6
Sean Koo <koo.sean@gmail.com>
2016-10-07 20:26:26 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -31,8 +31,36 @@ def turn(board)
     move(board, index)
     display_board(board)
   else
+    puts "Invalid entry"
     turn(board)
   end
 end
 
+def open_positions(board)
+  index = 0
+  open_count = 0
+  while index<9
+    if !position_taken?(board,index)
+      open_count +=1
+    end
+    index+=1
+  end
+return open_count
+end
+
 # Define your play method below
+def play(board)
+  winner = 0
+  while winner==0 && open_positions(board) > 0
+    turn(board)
+    if position_taken?(board,0) && ((board[0]==board[1] && board[1]==board[2]) || (board[0]==board[3] && board[3]==board[6]) || (board[0]==board[4] && board[4]==board[8]))
+      winner = 1
+    elsif position_taken?(board,4) && ((board[1]==board[4] && board[4]==board[7]) || (board[3]==board[4] && board[4]==board[5]) || (board[2]==board[4] && board[4]==board[6]))
+      winner = 1
+    elsif position_taken?(board,8) && ((board[2]==board[5] && board[5]==board[8]) || (board[6]==board[7] && board[7]==board[8]))
+      winner = 1
+    else
+      winner=0
+    end
+  end
+end
